81a27025a8f9d4ea607d5d4c51747f742b6289e7
GustavoLimaLuz <gustavooldl@gmail.com>
2022-03-30 20:52:29 -0300

    :tada: create cardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function CardGame(){
+    return `
+    <article class="card-game">
+        <img src="./src/imgs/card_back.png" alt="card back side">
+    </article>
+    `;
+}
+
+export default CardGame;
