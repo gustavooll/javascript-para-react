eb7c81a7320c8131e2b2cb2c23bae3834dc0f252
GustavoLimaLuz <gustavooldl@gmail.com>
2022-04-19 19:18:03 -0300

    :lipstick: add max-width to body and
    finished boardgame object

--- src/objects/boardGame/index.js
+++ src/objects/boardGame/index.js
@@ -1,13 +1,17 @@
+import "./style.css"
 import CardGame from "../../components/CardGame";
 
 function boardGame(how_many_cards){
 
-    let $htmlBoardGame = CardGame().repeat(how_many_cards);
+    let $htmlContent = CardGame().repeat(how_many_cards);
     // for (let i = 0; i < 6; i++) {
     //     $htmlBoardGame += CardGame();
     // }
     
-    return $htmlBoardGame;
+    return `
+    <section class="board-game">
+    ${$htmlContent}
+    </section>`;
 }
 
 export default boardGame;
